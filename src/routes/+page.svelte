eed562e7af617e7b23f9b6d37046d9409359e927
JHAhmed <jamalhascientist@gmail.com>
2024-11-18 16:00:35 +0530

    AR.js implementation first test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script>
-
 </script>
 
-<div class="flex items-center h-screen justify-center">
-    <h2 class="font-sans font-extralight text-5xl">Hi, there!</h2>
+<div class="flex flex-col h-screen space-y-8 items-center justify-center">
+	<h2 class="font-sans text-5xl font-extralight">Hi, there!</h2>
+	<a href="/arjs" class="px-4 py-2 border-2 bg-white rounded-full border-black">AR.js Preview</a>
+	<!-- <a href="/ar" class="px-4 py-2 border-2 bg-white rounded-full border-black">Augmented Reality Preview</a> -->
 </div>
---
+++ src/routes/arjs/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import model from '$lib/model.glb';
+</script>
+
+<div>
+	<a-scene embedded arjs>
+		<a-entity gltf-model={model} scale="0.5 0.5 0.5" position="0 0 0"> </a-entity>
+		<a-camera position="0 0 5"></a-camera>
+	</a-scene>
+</div>
