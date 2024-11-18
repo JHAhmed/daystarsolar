c9210f18763bdc4da26c8121b562e02ccb51992d
JHAhmed <jamalhascientist@gmail.com>
2024-11-18 20:19:38 +0530

    a-frame test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,6 @@
 <div class="flex flex-col h-screen space-y-8 items-center justify-center">
 	<h2 class="font-sans text-5xl font-extralight">Hi, there!</h2>
 	<a href="/arjs" class="px-4 py-2 border-2 bg-white rounded-full border-black">AR.js Preview</a>
+	<a href="/aframe" class="px-4 py-2 border-2 bg-white rounded-full border-black">A-Frame Preview</a>
 	<!-- <a href="/ar" class="px-4 py-2 border-2 bg-white rounded-full border-black">Augmented Reality Preview</a> -->
 </div>
---
+++ src/routes/aframe/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    // import 'aframe';
+    import model from '$lib/model.glb';
+
+</script>
+
+<div class="h-screen w-screen">
+    <a-scene embedded arjs>
+        <!-- Solar panel 3D model -->
+        <a-entity
+          gltf-model={model}
+          scale="0.5 0.5 0.5"
+          position="0 0 0">
+        </a-entity>
+      
+        <!-- Ground plane (optional) -->
+        <a-plane
+          position="0 0 0"
+          rotation="-90 0 0"
+          width="10"
+          height="10"
+          color="#7BC8A4">
+        </a-plane>
+      
+        <!-- AR Camera -->
+        <a-camera position="0 0 0"></a-camera>
+      </a-scene>
+      
+</div>
