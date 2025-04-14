f4dd26332fd1da7d2fe6ec986669e80e9511c9ec
JHAhmed <jamalhascientist@gmail.com>
2025-04-14 09:30:02 +0530

    Commented out error, AR Test

--- src/routes/api/updateEmail/+server.js
+++ src/routes/api/updateEmail/+server.js
@@ -1,34 +1,34 @@
-import { createClient } from 'contentful-management';
-import { env } from '$env/dynamic/private';
-import { json } from '@sveltejs/kit';
+// import contentfulManagement from 'contentful-management';
+// import { env } from '$env/dynamic/private';
+// import { json } from '@sveltejs/kit';
 
-export async function POST({ request }) {
-	const { emailId } = await request.json();
+// export async function POST({ request }) {
+// 	const { emailId } = await request.json();
 
-	try {
-		const client = createClient({
-			accessToken: env.CONTENTFUL_MANAGEMENT_TOKEN
-		});
+// 	try {
+// 		const client = contentfulManagement.createClient({
+// 			accessToken: env.CONTENTFUL_MANAGEMENT_TOKEN
+// 		});
 
-		const space = await client.getSpace(env.CONTENTFUL_SPACE_ID);
-		const environment = await space.getEnvironment('master');
+// 		const space = await client.getSpace(env.CONTENTFUL_SPACE_ID);
+// 		const environment = await space.getEnvironment('master');
 
-		const entry = await environment.createEntry('mailingList', {
-			fields: {
-				emailId: {
-					'en-US': emailId
-				}
-			}
-		});
+// 		const entry = await environment.createEntry('mailingList', {
+// 			fields: {
+// 				emailId: {
+// 					'en-US': emailId
+// 				}
+// 			}
+// 		});
 
-		await entry.publish();
+// 		await entry.publish();
 
-		return json({ success: true, message: 'Email added successfully' });
-	} catch (error) {
-		console.error('Error updating mailing list:', error);
-		return json(
-			{ success: false, message: 'Failed to add email', error: error.message },
-			{ status: 500 }
-		);
-	}
-}
+// 		return json({ success: true, message: 'Email added successfully' });
+// 	} catch (error) {
+// 		console.error('Error updating mailing list:', error);
+// 		return json(
+// 			{ success: false, message: 'Failed to add email', error: error.message },
+// 			{ status: 500 }
+// 		);
+// 	}
+// }
